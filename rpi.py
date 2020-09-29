5ed2ff2dad08d878f9a0b4f004149ee48856258b
Nixxos <nixxos123@gmail.com>
2020-09-29 16:48:18 +0100

    annotated possible RPI seial commands (readline/write) simple but necessary

--- rpi.py
+++ rpi.py
@@ -4,6 +4,8 @@ import serial
 #Establish Connection to RPI
 try:
     usb = serial.Serial(USB_PORT, 9600, timeout=2)
+    #usb.write(b'')
+    #usb.readline()
 except:
     print("error connecting")
     exit()
