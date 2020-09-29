8696b4055da18a42bf11ccb45be28ce9a0296a3b
Nixxos <nixxos123@gmail.com>
2020-09-29 11:37:12 -0400

    initial serial(USB) communication from RPI to Arduino

---
+++ rpi.py
@@ -0,0 +1,19 @@
+import serial
+
+
+#Establish Connection to RPI
+try:
+    usb = serial.Serial(USB_PORT, 9600, timeout=2)
+except:
+    print("error connecting")
+    exit()
+
+#Execution Loop
+while True:
+    command = input("Enter command: ")
+    if command == "a":  # read Arduino A0 pin value
+        print("Arduino A0 value:", value)
+    elif command == "x":  # exit program
+        print("Exiting program.")
+    else:
+        exit()
