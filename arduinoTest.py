ed51854c7799d1f4af532bf08a0479931cd79eaf
Nixxos <nixxos123@gmail.com>
2020-10-09 15:00:19 -0400

    offer second rpi arduion interface

--- arduinoTest.py
+++
@@ -1,6 +0,0 @@
-import serial
-import RPi.GPIO as GPIO
-import time
-ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
-
-
--- rpi.py
+++ rpi.py
@@ -19,3 +19,11 @@ while True:
         print("Exiting program.")
     else:
         exit()
+
+
+'''
+import serial
+import RPi.GPIO as GPIO
+import time
+ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+'''
